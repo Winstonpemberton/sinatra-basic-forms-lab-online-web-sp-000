413967e3ec1f975904edb1909f6de2328b70a953
Winston Pemberton <winstonpemberton@gmail.com>
2020-03-23 22:52:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,7 +12,13 @@ class App < Sinatra::Base
 
   post '/puppy' do
     @puppy = Puppy.new(params[:name],params[:breed],params[:age])
+<<<<<<< HEAD
     erb :display_puppy
   end
 
+=======
+  end
+
+
+>>>>>>> ef1f1f56c53564c1827e0a3236fed3afc8b335c9
 end
